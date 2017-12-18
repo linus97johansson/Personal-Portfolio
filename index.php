ce6b4ef8833a7e372a19e24c413bec446597059d
linus97johansson <linus97johansson@gmail.com>
2017-12-18 20:07:12 +0100

    Initial push

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include_once "php/config.php";
+    ?>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+<?php
+include_once "php/menu.php";
+?>
+</body>
+<script></script>
+</html>
---
+++ php/menu.php
@@ -0,0 +1,5 @@
+<ul>
+    <a href="" clsass><li>Home</li></a>
+    <a href=""><li>Websites</li></a>
+    <a href=""><li>Graphical</li></a>
+</ul>
