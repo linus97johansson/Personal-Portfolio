869aa1d0a261cc907b4825c98bbeb5b60b815bcb
linus97johansson <linus97johansson@gmail.com>
2017-12-30 19:20:30 +0100

    Email function up and working!

--- public/js/emailHandler.js
+++ public/js/emailHandler.js
@@ -1,6 +1,49 @@
+if (window.XMLHttpRequest) {
+    // code for modern browsers
+    xmlhttp = new XMLHttpRequest();
+} else {
+    // code for old IE browsers
+    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
 
-function sendEmail(){
+(function($) {
+    $.fn.autosubmit = function() {
+        this.submit(function(event) {
+            event.preventDefault();
+            emptyForm();
+            var form = $(this);
+            $.ajax({
+                type: form.attr('method'),
+                url: form.attr('action'),
+                data: form.serialize()
+            }).done(function(data) {
+                formSent(true);
+            }).fail(function(data) {
+                formSent(false);
+            });
+        });
+        return this;
+    }
+})(jQuery)
 
+$(function() {
+    $('form[data-autosubmit]').autosubmit();
+});
 
+function formSent(boolean){
 
+    if(boolean == true){
+        //alert("The email was sent!");
+        document.getElementById("submited").style.zIndex = "10";
+        document.getElementById("submited").style.opacity = "1";
+
+    }
+    if(boolean == false){
+
+    }
+
+}
+function emptyForm(){
+    document.getElementById("sub").disabled = true;
+    document.getElementById("contactForm").reset();
 }
--- test.js
+++ test.js
@@ -11,19 +11,63 @@ app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+
+// Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
+app.listen(bodyParser.testButton)
+function test(){
+    console.log("This could theoretically send the email");
+}
 
-app.post('/send', (res, req) => {
+app.post('/send', (req, res) => {
 
     console.log(req.body);
+
+    const output = `
+        <p>New Message!</p>
+        <h3>Details:</h3>
+        <ul>
+        <li>email:${req.body.email}</li>
+        <li>phone:${req.body.phone}</li>
+</ul>
+<h3>Message:</h3>
+<p>${req.body.message}</p>
+    `;
+
+let transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+        user: "linus97johansson@gmail.com", // generated ethereal user
+        pass: "TrollinWolf" // generated ethereal password
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Test! 👻" <linus97johansson@gmail.com>', // sender address
+    to: 'bloodedmaniac@gmail.com', // list of receivers
+    subject: ' | new message !', // Subject line
+    html: output // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+res.render('index', {msg: "Email Sent!"});
+
+});
 
 });
 
